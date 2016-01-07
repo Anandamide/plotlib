24247ef7502626a758e8bf107edd0c1d0da56e50
086709 <Jordan.jacobson11@gmail.com>
2016-01-07 00:20:43 -0700

    Examples
    Two sample plots. Plot one is a contour of the vector field f(x y) = sin(x)i + cos(y)j along the line f(x) = sin(x) over the interval { -5 <= x < 5}. Plot two is a plot of the vector field f(x y) = xi -yj

---
+++ sample-plots.scm
@@ -0,0 +1,46 @@
+;plots a few sample plots
+(define (refresh device)
+ (clear device "white")
+ (coord-grid-cart device "black"))
+
+(define plot1 (make-graphics-device 'win32))
+
+
+(refresh plot1)
+
+
+(draw-line plot1 "blue" (make-line-cords
+			(scale-point-list
+			  (zip (range -5 5 .01)
+			       (map sin
+				    (range -5 5 .01)))
+		         .2 .8)))
+
+(draw-vector-list plot1 "red"
+	(pair-zip
+	  (scale-point-list
+	    (zip (range -5 5 .1)
+	         (map sin (range -5 5 .1)))
+		  .2 .8)
+		  (zip (map (lambda (x)
+			            (* .2 (sin x)))
+			       (range -5 5 .1))
+			     (map (lambda (x)
+			            (* .2 (cos x)))
+			       (range -5 5 .1)))))
+
+(label plot1 "black" "Plot 1" 0 .9)
+
+(define plot2 (make-graphics-device 'win32))
+
+(refresh plot2)
+
+(vector-field-plot plot2 "purple" 
+                   (range -1 1 .1) (range -1 1 .1)
+				           (lambda (x y) 
+				             (cons (* .1 y)
+						           (* -.1 x))))
+
+(label plot2 "black" "Plot 2" 0 .9)
+
+
