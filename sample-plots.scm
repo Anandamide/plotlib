1349429fa5c4a80a913c682c2b757872fe1b2be6
086709 <Jordan.jacobson11@gmail.com>
2016-01-07 00:23:48 -0700

    Update sample-plots.scm

--- sample-plots.scm
+++ sample-plots.scm
@@ -22,12 +22,12 @@
 	    (zip (range -5 5 .1)
 	         (map sin (range -5 5 .1)))
 		  .2 .8)
-		  (zip (map (lambda (x)
-			            (* .2 (sin x)))
-			       (range -5 5 .1))
-			     (map (lambda (x)
-			            (* .2 (cos x)))
-			       (range -5 5 .1)))))
+	  (zip (map (lambda (x)
+	              (* .2 (sin x)))
+	            (range -5 5 .1))
+	       (map (lambda (x)
+	              (* .2 (cos x)))
+	            (range -5 5 .1)))))
 
 (label plot1 "black" "Plot 1" 0 .9)
 
@@ -37,9 +37,9 @@
 
 (vector-field-plot plot2 "purple" 
                    (range -1 1 .1) (range -1 1 .1)
-				           (lambda (x y) 
-				             (cons (* .1 y)
-						           (* -.1 x))))
+		   (lambda (x y) 
+		     (cons (* .1 y)
+		           (* -.1 x))))
 
 (label plot2 "black" "Plot 2" 0 .9)
 
